119717d64af01962cc9a9719a1266de2e71ca410
jinhwaPark <parkjinhwa3914@gmail.com>
2022-03-25 19:41:26 +0900

    use DOM to save HTML Object

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+// DOM = Document Object Model
+const id = document.querySelector("#id"),
+    pwd = document.querySelector("#pwd"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pwd: pwd.value
+    };
+    console.log(req);
+
+    // fetch();
+}
